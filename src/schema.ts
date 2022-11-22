86d39935c1feb399ba025e8e1d7f3e38b42ebc87
Tommy Zarick <zarick.6@osu.edu>
2022-11-22 15:55:29 -0500

    feat: simple schema that works with some queries

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,9 @@
 import { makeSchema } from 'nexus';
+import * as types from "./graphql";
 import { join } from 'path';
 
 export const schema = makeSchema({
-  types: [],
+  types,
   outputs: {
     schema: join(process.cwd(), "schema.graphql"),
     typegen: join(process.cwd(), "nexus-typegen.ts")
